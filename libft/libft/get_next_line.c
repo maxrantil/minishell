ac15222a0636af010d19aca5191efe8a22d2c241
mqx <litnar@pm.me>
2022-10-01 18:29:52 +0300

    thanks leo

--- libft/libft/get_next_line.c
+++ libft/libft/get_next_line.c
@@ -12,75 +12,64 @@
 
 #include "../includes/get_next_line.h"
 
-static int	manage_str(int fd, char **stat_buf, char **line)
+static int	get_line(char **stat_str, char **line, int fd, int i)
 {
 	char	*temp;
+	int		len;
 
-	*line = ft_strsub(stat_buf[fd], 0, ft_strclen(stat_buf[fd], '\n'));
-	if (ft_strchr(stat_buf[fd], '\n'))
-		temp = ft_strdup(ft_strchr(stat_buf[fd], '\n') + 1);
-	ft_strdel(&stat_buf[fd]);
-	stat_buf[fd] = ft_strdup(temp);
-	ft_strdel(&temp);
-	return (1);
+	len = 0;
+	if (ft_strchr(stat_str[fd], '\n') != NULL)
+	{
+		while (stat_str[fd][len] != '\n')
+			len++;
+		*line = ft_strsub(stat_str[fd], 0, len);
+		temp = ft_strdup(ft_strchr(stat_str[fd], '\n') + 1);
+		ft_strdel(&stat_str[fd]);
+		stat_str[fd] = temp;
+		i = 1;
+	}
+	else if (i == 0)
+	{
+		*line = ft_strdup(stat_str[fd]);
+		ft_strdel(&stat_str[fd]);
+		if (*line[0] != '\0')
+			i = 1;
+	}
+	return (i);
 }
 
-static int	work_it(int fd, char **stat_buf, char *read_buf, char **line)
+static int	cpy_to_static(char **stat_str, char *buffer, int fd)
 {
-	char	*temp;
-
-	temp = ft_strsub(read_buf, 0, ft_strclen(read_buf, '\n'));
-	stat_buf[fd] = ft_strupdate(stat_buf[fd], temp);
-	ft_strdel(&temp);
-	temp = ft_strdup(ft_strchr(read_buf, '\n') + 1);
-	*line = ft_strdup(stat_buf[fd]);
-	ft_strdel(&stat_buf[fd]);
-	stat_buf[fd] = ft_strdup(temp);
-	ft_strdel(&temp);
-	return (2);
-}
-
-static int	read_file(int fd, char **stat_buf, char *read_buf, char **line)
-{
-	int				ret;
-
-	ret = read(fd, read_buf, BUFF_SIZE);
-	if (ret == -1)
-		return (-1);
-	read_buf[ret] = '\0';
-	if (ft_strchr(read_buf, '\n') == NULL)
-		stat_buf[fd] = ft_strupdate(stat_buf[fd], read_buf);
+	if (stat_str[fd] != NULL)
+		stat_str[fd] = ft_strupdate(stat_str[fd], buffer);
 	else
-		return (work_it(fd, stat_buf, read_buf, line));
-	if (ret == 0)
-		return (0);
-	return (1);
+		stat_str[fd] = ft_strdup(buffer);
+	return (2);
 }
 
 int	get_next_line(const int fd, char **line)
 {
-	char			read_buf[BUFF_SIZE + 1];
-	static char		*stat_buf[FD_MAX];
-	int				ret;
+	static char	*stat_str[FD_MAX];
+	char		*buffer;
+	int			i;
 
-	ret = 1;
-	if (!line || fd < 0 || fd > FD_MAX || BUFF_SIZE <= 0)
-		return (-1);
-	if (!stat_buf[fd])
-		stat_buf[fd] = ft_strnew(BUFF_SIZE);
-	if (ft_strchr(stat_buf[fd], '\n') != NULL)
-		return (manage_str(fd, stat_buf, line));
-	while (ret)
+	i = 2;
+	buffer = (char *)malloc(sizeof(char) * (BUFF_SIZE + 1));
+	if (fd < 0 || line == NULL || BUFF_SIZE <= 0 || fd >= FD_MAX)
+		i = -1;
+	while (i >= 0)
 	{
-		ret = read_file(fd, stat_buf, read_buf, line);
-		if (ret == -1)
-			return (-1);
-		if (ret == 2)
-			return (1);
+		if (stat_str[fd] != NULL)
+			i = get_line(stat_str, line, fd, i);
+		if (i == 1 || i == 0)
+			break ;
+		i = read(fd, buffer, BUFF_SIZE);
+		if (i > 0)
+		{
+			buffer[i] = '\0';
+			i = cpy_to_static(stat_str, buffer, fd);
+		}
 	}
-	*line = ft_strdup(stat_buf[fd]);
-	ft_strdel(&stat_buf[fd]);
-	if (ft_strlen(*line) > 0)
-		return (1);
-	return (0);
+	ft_strdel(&buffer);
+	return (i);
 }
