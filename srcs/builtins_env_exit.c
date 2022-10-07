a6ace625439bc3d48c1d725bfeccfff6f9c4a3f5
Max <rantil@pm.me>
2022-10-07 15:57:15 +0300

    no leaks from env now

--- srcs/builtins_env_exit.c
+++ srcs/builtins_env_exit.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 13:33:14 by mrantil           #+#    #+#             */
-/*   Updated: 2022/10/07 14:41:32 by mrantil          ###   ########.fr       */
+/*   Updated: 2022/10/07 15:56:33 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static char	**switch_args(char **args, size_t len, size_t i)
 	char	**new_args;
 	size_t	j;
 
-	new_args = (char **)ft_memalloc(sizeof(char *) * len);
+	new_args = (char **)ft_memalloc(sizeof(char *) * (ft_arrlen((void **)&args[i]) + 1));
 	if (!new_args)
 		print_error(3);
 	j = 0;
@@ -47,7 +47,6 @@ static char	**set_tempenv(t_msh *msh, size_t i)
 	temp_env[x] = NULL;
 	return (temp_env);
 }	
-
 
 int	msh_env(t_msh *msh)
 {
@@ -82,7 +81,7 @@ int	msh_env(t_msh *msh)
 		msh->temp_env = set_tempenv(msh, i);
 		while (ft_strchr(msh->args[i], '='))
 			i++;
-		msh->args = switch_args(msh->args, arrlen - 1, i);
+		msh->args = switch_args(msh->args, arrlen, i);
 	}
 	msh_launch(msh);
 	return (1);
