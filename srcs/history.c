9851d47f0333c3779b7212c6afd4d927bb79d0fa
Max <rantil@pm.me>
2022-10-17 10:00:06 +0300

    need to read into 2d array

--- srcs/history.c
+++ srcs/history.c
@@ -1,6 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   history.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/17 09:38:04 by mrantil           #+#    #+#             */
+/*   Updated: 2022/10/17 09:38:57 by mrantil          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "msh.h"
 
-static 	void	write_to_history(t_msh *msh)
+/*
+**	O_CREAT If pathname does not exist, create it as a regular file.
+**	O_RDWR access modes: read/write
+**	O_APPEND The  file  is  opened  in append mode.
+**	S_IRWXU  00700 user (file owner) has read, write, and execute permissionS_IRUSR  00400 user has read permission	
+*/
+
+static void	write_to_history(t_msh *msh)
 {
 	int		fd;
 
@@ -10,7 +29,7 @@ static 	void	write_to_history(t_msh *msh)
 	close(fd);
 }
 
-static void	print_history()
+static void	print_history(void)
 {
 	char	*buf;
 	int		fd;
@@ -24,7 +43,7 @@ static void	print_history()
 	close(fd);
 }
 
-static size_t	history_count()
+static size_t	history_count(void)
 {
 	char	*buf;
 	size_t	line_numbers;
@@ -54,19 +73,12 @@ static void	print_history_of_choice(size_t line_numbers, size_t line)
 		if (!line)
 			break ;
 		ft_strdel(&buf);
-		
 	}
 	ft_putendl(buf);
 	ft_strdel(&buf);
 	close(fd);
 }
 
-/*
-**	O_CREAT If pathname does not exist, create it as a regular file.
-**	O_RDWR access modes: read/write
-**	O_APPEND The  file  is  opened  in append mode.
-**	S_IRWXU  00700 user (file owner) has read, write, and execute permissionS_IRUSR  00400 user has read permission	
-*/
 void	history(t_msh *msh, int status)
 {
 	if (status == 1 && msh->cl[0] == 'h' && msh->cl[1] == 'i' && msh->cl[2] == 's' && msh->cl[3] == 't' && msh->cl[4] == 'o' && msh->cl[5] == 'r' && msh->cl[6] == 'y')
