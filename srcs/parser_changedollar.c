bb8b68b764db76cdff3ee2913b1b84836c89c4e5
Max <rantil@pm.me>
2022-10-17 12:06:21 +0300

    ready for eval monday

--- srcs/parser_changedollar.c
+++ srcs/parser_changedollar.c
@@ -6,7 +6,7 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 12:33:11 by mrantil           #+#    #+#             */
-/*   Updated: 2022/10/12 12:11:02 by mrantil          ###   ########.fr       */
+/*   Updated: 2022/10/17 10:13:16 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,12 +76,7 @@ static char	*get_new_arg(t_msh *msh, char **dollars)
 				change_new_arg(msh, &new_arg, i);
 		}
 		if (end_arg)
-		{
-			// if (new_arg)
-				new_arg = ft_strupdate(new_arg, end_arg);
-			// else
-				// new_arg = ft_strdup(end_arg);
-		}
+			new_arg = ft_strupdate(new_arg, end_arg);
 		ft_strdel(&end_arg);
 		ft_strdel(&key);
 	}
@@ -101,9 +96,7 @@ void	get_dollar(t_msh *msh, char *dollar, size_t i)
 	ft_strdel(&msh->args[i]);
 	if (begin_arg && new_arg)
 		msh->args[i] = ft_strupdate(begin_arg, new_arg);
-	else if (new_arg)
+	else
 		msh->args[i] = ft_strdup(new_arg);
-	else if (begin_arg)
-		msh->args[i] = begin_arg;
 	ft_strdel(&new_arg);
 }
