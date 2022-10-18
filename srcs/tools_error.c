6eef5bfa426acb30372d7bd21dfc43647e3167f5
Max <rantil@pm.me>
2022-10-18 15:27:20 +0300

    eval 4

--- srcs/tools_error.c
+++ srcs/tools_error.c
@@ -6,20 +6,28 @@
 /*   By: mrantil <mrantil@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 19:45:05 by mrantil           #+#    #+#             */
-/*   Updated: 2022/10/12 16:32:22 by mrantil          ###   ########.fr       */
+/*   Updated: 2022/10/18 14:52:53 by mrantil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "msh.h"
 
-static void	print_status(int status)
+static void	print_status(int status, char *arg)
 {
+	struct stat	statbuf;
+
 	if (status == -1)
 		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
 	if (status == -2)
 		ft_putstr_fd(": Permission denied\n", STDERR_FILENO);
 	if (status == 0)
-		ft_putstr_fd(": Is a directory\n", STDERR_FILENO);
+	{
+		lstat(arg, &statbuf);
+		if (S_ISDIR(statbuf.st_mode))
+			ft_putstr_fd(": Is a directory\n", STDERR_FILENO);
+		else
+			ft_putstr_fd(": Exec format error\n", STDERR_FILENO);
+	}
 }
 
 static int	check_address(char *addr)
@@ -45,7 +53,7 @@ static void	print_usage(char *arg, int status)
 	if (!ft_strchr(arg, '/'))
 		ft_putstr_fd(": command not found\n", STDERR_FILENO);
 	else
-		print_status(status);
+		print_status(status, arg);
 }
 
 static void	print_setenv_error(char *arg)
@@ -74,7 +82,7 @@ void	print_error(char *arg, int i)
 	else if (i == 5)
 	{
 		ft_printf("minishell: cd: %s", arg);
-		print_status(status);
+		print_status(status, arg);
 	}
 	else if (i == 6)
 		print_setenv_error(arg);
